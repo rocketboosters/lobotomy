30efe7634fbddcbcc303c4dffd29a40982224794
Scott Ernst <swernst@gmail.com>
2020-11-12 22:03:44 -0600

    S3 Downloads
    Adds augmentation for s3.download_file and s3.download_file_obj,
    neither of which have botocore definitions as they are boto3
    add-ons.

---
+++ lobotomy/tests/edge_cases/test_s3_download_file.py
@@ -0,0 +1,10 @@
+import lobotomy
+
+
+@lobotomy.Patch()
+def test_s3_download_file(lobotomized: lobotomy.Lobotomy):
+    """Should handle s3.download_file correctly despite it being an augmentation."""
+    lobotomized.data = {"clients": {"s3": {"download_file": {}}}}
+    session = lobotomized()
+    client = session.client("s3")
+    client.download_file(Filename="foo", Bucket="bar", Key="baz")
