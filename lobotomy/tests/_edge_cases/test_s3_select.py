f5ee1a8998ff9bd525067410092a228b55074918
Scott Ernst <swernst@gmail.com>
2021-09-28 10:34:27 -0500

    S3 Select Expanded Tests
    Adds additional S3 select testing.

--- lobotomy/tests/_edge_cases/test_s3_select.py
+++ lobotomy/tests/_edge_cases/test_s3_select.py
@@ -1,4 +1,8 @@
+import pathlib
+
 import lobotomy
+
+_directory = pathlib.Path(__file__).resolve().parent
 
 
 @lobotomy.patch()
@@ -33,4 +37,26 @@ def test_s3_select(lobotomized: lobotomy.Lobotomy):
         {"Records": {"Payload": b"a"}},
         {"Records": {"Payload": b"b"}},
         {"Records": {"Payload": b"c"}},
+    ]
+
+
+@lobotomy.patch(_directory.joinpath("test_s3_select.yaml"))
+def test_s3_select_from_yaml(lobotomized: lobotomy.Lobotomy):
+    """Should correctly return S3 Select results."""
+    response = (
+        lobotomized()
+        .client("s3")
+        .select_object_content(
+            Bucket="foo",
+            Key="bar/baz.file",
+            InputSerialization={"Parquet": {}},
+            Expression="SELECT * FROM S3Object LIMIT 100",
+            ExpressionType="SQL",
+            OutputSerialization={"JSON": {"RecordDelimiter": "\n"}},
+        )
+    )
+    observed = list(response["Payload"])
+    assert observed == [
+        {"Records": {"Payload": b'{"foo": "bar", "spam": 1000}'}},
+        {"Records": {"Payload": b'{"foo": "baz", "spam": 2000}'}},
     ]
